c2589a5ba0867bc9b5703fdf06dffd37be35c30b
Sanshain <digital-mag@ya.ru>
2020-03-01 18:03:58 +0300

    'start'

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            
+            var views = new StackLayout();
+            var scroller = new ScrollView { Content = views };
+            var btn = new Button { Text = "Click" };
+            var rootLayout = new AbsoluteLayout();
+            
+            rootLayout.Children.Add(scroller, new Rectangle(0, 0, 1, 0.9));
+
+            
+            AbsoluteLayout.SetLayoutFlags(scroller, AbsoluteLayoutFlags.SizeProportional);
+            //*/
+            rootLayout.Children.Add(btn, new Rectangle(0, 1, 1, 0.1));
+            AbsoluteLayout.SetLayoutFlags(btn, AbsoluteLayoutFlags.All);//*/
+
+            Title = "Absolute Layout Exploration - Code";
+
+            Content = rootLayout;
+            
+            for (int i = 0; i < 35; i++)
+            {
+                views.Children.Add(new Label {
+                    Text = "Hello " + i,
+                    FontSize = 16
+                });
+            }
+
+
+            /*
+            .Children.Add(new Button
+            {
+                Text = "Click",
+                FontSize = 14
+            });//*/
+
+            /*
+            {
+                Children = {
+                    new Label { Text = "Hello Page" },
+                }
+            };//*/
+
+
+        }
+	}
+}
