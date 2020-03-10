791fc23ec254abd52db801d40ef17ef492d2bd32
Test <digital-mag@ya.ru>
2020-03-10 11:13:45 +0300

    garbage cleaning

--- XxmsApp/XxmsApp/Views/CreateNote.xaml.cs
+++ XxmsApp/XxmsApp/Views/CreateNote.xaml.cs
@@ -29,103 +29,87 @@ namespace XxmsApp.Views
 
 			InitializeComponent ();
 
-            
-            // var h = Device.GetNamedSize(NamedSize.Large, typeof(Entry)) * 1.5;
-
-
-
-            var container = new StackLayout();
-
-
-            msgFields = new StackLayout()
-            {
+            var container = new StackLayout();            
+            msgFields = new StackLayout(){                                              // Initialize message Forms
                 VerticalOptions = LayoutOptions.FillAndExpand,
-                BackgroundColor = Color.LightGray
-            };
+                BackgroundColor = Color.LightGray};
 
-            var pageHeight = ((Application.Current.MainPage as MasterDetailPage).Detail as NavigationPage).RootPage.Height;
-            var entryHeight = Device.GetNamedSize(NamedSize.Default, typeof(Entry));
-
-            var adresseeEntry = new Entry { BackgroundColor = Color.LightCoral, VerticalOptions = LayoutOptions.Start };
+            var adresseeEntry = new Entry {
+                BackgroundColor = Color.LightCoral,
+                VerticalOptions = LayoutOptions.Start };
             msgFields.Children.Add(adresseeEntry);
-            /*Constraint.Constant(0),
-            Constraint.Constant(0),
-            Constraint.RelativeToParent(p => p.Width),
-            Constraint.Constant(16));
-            // new Rectangle(0, 0, 1, 0.1), AbsoluteLayoutFlags.All);//*/
 
             var messageEditor = new Editor {
                 BackgroundColor = Color.Blue,
                 Margin = new Thickness(5),
                 VerticalOptions = LayoutOptions.FillAndExpand };
             msgFields.Children.Add(messageEditor);
-            /*
-            Constraint.Constant(0),
-            Constraint.Constant(0),
-            Constraint.RelativeToParent(p => p.Width),
-            Constraint.RelativeToView(container, (v, p) => v.Height * 0.8));
-            //new Rectangle(0, 0.1, 1, 0.9), AbsoluteLayoutFlags.All);//*/
 
             container.Children.Add(msgFields);
 
-            if (Device.RuntimePlatform == Device.iOS)
+            
+            bottom = new StackLayout { VerticalOptions = LayoutOptions.Center };        // Initialize Button for send
+            var send = new Button { Text = "Send" };
+            bottom.Children.Add(send);
+            send.Clicked += Send_Clicked;
+            container.Children.Add(bottom);
+
+            
+            Content = container;                                                        // set container
+
+
+            /// if its not Android, set `Focused`
+            if (Device.RuntimePlatform == Device.iOS)  
             {
                 messageEditor.Focused += MessageEditor_Focused;
-                messageEditor.Unfocused += MessageEditor_Focused;
+                messageEditor.Unfocused += MessageEditor_Unfocused;
                 adresseeEntry.Focused += MessageEditor_Focused;
-                adresseeEntry.Unfocused += MessageEditor_Focused;
+                adresseeEntry.Unfocused += MessageEditor_Unfocused;
             }
-
-            // container.Children.Add(new StackLayout { VerticalOptions = LayoutOptions.CenterAndExpand });
-            var send = new Button { Text = "Send" };
-            bottom = new StackLayout { VerticalOptions = LayoutOptions.Center };
-            bottom.Children.Add(send);
-            send.Clicked += (object sender, EventArgs e) =>
-            {
-                
-                DisplayAlert(
-                    msgFields.Height.ToString(), 
-                    container.Height.ToString(), 
-                    Application.Current.MainPage.Height.ToString() + " : " + pageHeight.ToString(),
-                    entryHeight.ToString() + ":" + msgFields.Children[0].Height + ":" + send.Height);
-                //*/
-            };
-
-            container.Children.Add(bottom);
-            
-            Content = container;//*/
 
         }
 
+
         private void MessageEditor_Unfocused(object sender, FocusEventArgs e)
         {
-            
+            bottom.HeightRequest = -1;
         }
 
         private void MessageEditor_Focused(object sender, FocusEventArgs e)
         {
-            
-            if (e.IsFocused) {
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+
                 var pageHeight = ((Application.Current.MainPage as MasterDetailPage).Detail as NavigationPage).RootPage.Height;
-                bottom.HeightRequest = pageHeight * 0.55;
-            }            
-            else
-                bottom.HeightRequest = -1;
+                bottom.HeightRequest = pageHeight * 0.55; // nearly
+
+                return false;
+            });                
 
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
-            /*
-            var entryHeight = msgFields.Children.First().Height;
-            var pageHeight = ((Application.Current.MainPage as MasterDetailPage).Detail as NavigationPage).RootPage.Height;
-            var messageEditor = new Editor { BackgroundColor = Color.Blue, HeightRequest = pageHeight - entryHeight * 3 };
-            // messageEditor.Focused += MessageEditor_Focused;
-            msgFields.Children.Add(messageEditor);//*/
         }
 
+
+
+
+        [Obsolete("This yet just gag. Need to realize")]
+        private void Send_Clicked(object sender, EventArgs e)
+        {
+            var entryHeight = Device.GetNamedSize(NamedSize.Default, typeof(Entry));
+
+            var pageHeight = ((Application.Current.MainPage as MasterDetailPage).Detail as NavigationPage).RootPage.Height;
+
+            DisplayAlert(
+                msgFields.Height.ToString(),
+                 ((sender as Button).Parent.Parent as StackLayout).Height.ToString(),
+                Application.Current.MainPage.Height.ToString() + " : " + pageHeight.ToString(),
+                entryHeight.ToString() + ":" + msgFields.Children[0].Height + ":" + (sender as Button).Height);
+        }
 
     }
 }
